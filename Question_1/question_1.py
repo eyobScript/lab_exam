7241ba036668904fe4df183f593b4436e5ff4198
eyobScript <eyobbmulugeta@gmail.com>
2025-02-07 10:53:35 +0300

    Lab exam openGl

--- Question_1/question_1.py
+++ Question_1/question_1.py
@@ -2,3 +2,11 @@ def find_matrix_shape(matrix):
     Rows = len(matrix)
     Cols = len(matrix[0]) if matrix else 0
     return (Rows, Cols)
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(find_matrix_shape(matrix))
